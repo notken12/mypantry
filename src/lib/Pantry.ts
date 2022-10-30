03e48d5ebb261838911dd11482816ec1a2d43122
notken12 <kendotzhou@gmail.com>
2022-10-30 11:51:01 -0400

    add types

--- src/lib/Pantry.ts
+++ src/lib/Pantry.ts
@@ -1,19 +1,55 @@
 import { model, Schema } from 'mongoose';
 
-export type Item = {
-  _id: Id;
-  name: string;
-  amount: number;
-  imageURL: URL | null;
-};
 export type Id = string;
+export type Item = {
+	_id: Id;
+	name: string;
+	amount: number;
+	imageURL: URL | null;
+};
+
+export interface Operation {
+	type: string;
+	uid: Id | null;
+	data: Record<string, any>;
+	timestamp: Date;
+}
+
+export type EditItems = Operation & {
+	uid: Id;
+	type: 'EditItems';
+	data: {
+		changes: Record<Id, Partial<Item>>;
+	};
+};
+export type NewItem = Operation & {
+	uid: Id;
+	type: 'NewItem';
+	data: {
+		item: Item;
+	};
+};
+export type CheckoutItems = Operation & {
+	type: 'CheckoutItems';
+	uid: Id | null;
+	data: {
+		optionalInfo: {
+			firstName: string | null;
+			lastName: string | null;
+			additionalRemarks: string | null;
+		};
+		itemAmounts: Record<Id, number>;
+		approved: boolean;
+	};
+};
 
 export type Pantry = {
-  _id: Id;
-  name: string;
-  description: string;
-  createdDate: Date;
-  inventory: Item[];
-  owner: Id;
-  editors: Id[];
+	_id: Id;
+	name: string;
+	description: string;
+	createdDate: Date;
+	inventory: Item[];
+	owner: Id;
+	editors: Id[];
+	history: Operation[];
 };
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,44 +1,56 @@
 import { DB_KEY } from '$env/static/private';
 import mongoose, { Schema, model } from 'mongoose';
-import type { Pantry, Item } from './Pantry';
+import type { Pantry, Item, Operation } from './Pantry';
 import { nanoid } from 'nanoid';
 
 if (DB_KEY == undefined)
-  throw new Error("Error: couldn't find database key. Add DB_KEY=<database key here> to .env file");
+	throw new Error("Error: couldn't find database key. Add DB_KEY=<database key here> to .env file");
 await mongoose.connect(DB_KEY);
 
 const ItemSchema = new Schema<Item>({
-  _id: {
-    type: String,
-    default: () => nanoid()
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  amount: {
-    type:Number,
-    required:true
-  },
-  imageURL: {
-    type: String,
-    required: false
-  }
+	_id: {
+		type: String,
+		default: () => nanoid()
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	amount: {
+		type: Number,
+		required: true
+	},
+	imageURL: {
+		type: String,
+		required: false
+	}
 });
 
-const PantrySchema = new Schema<Pantry>({
-  _id: {
-    type: String,
-    default: () => nanoid(11)
-  },
-  name: String,
-  description: String,
-  createdDate: {
-    type: Date,
-    default: () => Date.now()
-  },
-  inventory: [ItemSchema],
-  owner: String,
-  editors: [String]
-});
+const OperationSchema = new Schema<Operation>(
+	{
+		type: {
+			type: String,
+			required: true
+		},
+		uid: String,
+		data: Object
+	},
+	{ timestamps: { createdAt: 'timestamp' } }
+);
+
+const PantrySchema = new Schema<Pantry>(
+	{
+		_id: {
+			type: String,
+			default: () => nanoid(11)
+		},
+		name: String,
+		description: String,
+		inventory: [ItemSchema],
+		owner: String,
+		editors: [String],
+		history: [OperationSchema]
+	},
+	{ timestamps: true }
+);
 export const PantryModel = model<Pantry>('Pantry', PantrySchema);
