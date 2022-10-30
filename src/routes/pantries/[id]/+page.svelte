3215a594939f920ddb035962d1d74d0d0350c8ea
Crazexpert <dev.velp@gmail.com>
2022-10-30 01:18:59 +0000

    security

--- src/routes/pantries/[id]/+page.svelte
+++ src/routes/pantries/[id]/+page.svelte
@@ -5,6 +5,7 @@
 	import { fetchAuthed } from '$lib/fetch';
 
 	import type { Pantry, Item } from '$lib/Pantry';
+	import { user } from '$lib/stores';
 
 	import type { PageData } from './$types';
 	import * as QRCode from 'qrcode';
@@ -26,6 +27,7 @@
 			body: JSON.stringify({ name, amount })
 		});
 		invalidateAll();
+		pantry.inventory = [...pantry.inventory];
 	};
 	let newPantryName = '';
 	let newPantryDesc = '';
@@ -40,6 +42,8 @@
 			body: JSON.stringify({ name, description })
 		});
 		invalidateAll();
+		pantry.name = name;
+		pantry.description = description;
 	};
 	let confirmDelete = '';
 	const deletePantry = async (e: Event) => {
@@ -54,34 +58,29 @@
 	};
 	let amountsToCheckOut: number[] = [];
 	let qrCodeImage: HTMLImageElement;
+	let editorStatus: boolean;
 	onMount(() => {
 		QRCode.toDataURL(window.location.href, (e, url) => {
 			qrCodeImage.src = url;
 		});
+		editorStatus = $user?.uid == pantry.owner; //edit this later for collaborators
 	});
 </script>
 
 <main>
 	<a href="/dashboard">Back to dashboard</a>
-	<form on:submit={() => deletePantry}>
-		<legend style="color:red;">Delete Pantry</legend>
-		<input
-			type="text"
-			placeholder="Type the name of this pantry to delete it"
-			bind:value={confirmDelete}
-		/>
-		<input type="submit" value="Delete" />
-	</form>
 	<h1>{pantry.name ?? 'Unnamed pantry'}</h1>
 	<p>{pantry.description ?? 'No description provided'}</p>
+	{#if editorStatus} 
 	<form on:submit={editPantryDesc}>
 		<input type="text" placeholder="New Pantry Name" bind:value={newPantryName} />
 		<input type="text" placeholder="New Pantry Description" bind:value={newPantryDesc} />
 		<input type="submit" value="Change" />
 	</form>
+	{/if}
 	<p>
-		Owner(s): {users[pantry.owner]?.displayName}<br />
-		Owner Id(s): {pantry.owner}
+		Owner: {users[pantry.owner]?.displayName}<br />
+		Collaborators: LATER my gosh ken can you do this
 	</p>
 	<h2>Items</h2>
 	<ul>
@@ -92,25 +91,42 @@
 			<br>
 		{/each}
 	</ul>
+	{#if editorStatus} 
 	<form on:submit={newItem}>
 		<legend>New Item</legend>
 		<input type="text" placeholder="Item name" bind:value={newItemName} />
 		<input type="number" placeholder="Amount" bind:value={amountOfItem} />
 		<input type="submit" value="New item" />
 	</form>
+	{/if}
 	<form>
 		<legend>Cart</legend>
 		{#if amountsToCheckOut.filter(Boolean).length != 0}
 		{#each pantry.inventory as cartItem, i}
 			{#if amountsToCheckOut[i]}
-				<img src={cartItem.imageURL?.href} alt="" width="50" />
+				<img src={cartItem.imageURL?.href} alt={cartItem.name + ' item image'} width="50" />
 				<li>{cartItem.name}: {amountsToCheckOut[i]}</li>
 			{/if}
 		{/each}
 		{:else} 
-			<p>Nothing in Cart currently!</p>
+			<p>Nothing in cart currently!</p>
 		{/if}
 		<input type="submit" value="Check Out">
 	</form>
-	<img bind:this={qrCodeImage} alt="QR code" width="200" />
+	{#if $user?.uid == pantry.owner} <!--this is only for owners-->
+	<form on:submit={() => deletePantry}>
+		<legend style="color:red;">Delete Pantry</legend>
+		<input
+			type="text"
+			placeholder="Type the name of this pantry to delete it"
+			bind:value={confirmDelete}
+		/>
+		<input type="submit" value="Delete" />
+	</form>
+	{/if}
+	<div id="share">
+		<h1>Share</h1>
+		<button on:click={()=>navigator.clipboard.writeText(window.location.href)}>Copy link</button> 
+		<img bind:this={qrCodeImage} alt="QR code" width="200" /><a href={qrCodeImage?.src} download>Download QRCode as image</a>
+	</div>
 </main>
